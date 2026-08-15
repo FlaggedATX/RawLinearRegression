04c9e78ad10cfd943fb8efd187a367ca65cb2017
FlaggedATX <renatosegundo840@gmail.com>
2026-08-15 01:08:34 -0300

    chore: Add comments to most of the code

--- dataset.py
+++ dataset.py
@@ -2,19 +2,23 @@ import numpy as np
 
 
 def make_df():
-    rng = np.random.default_rng(seed=None)
+    rng = np.random.default_rng(seed=None) #Only instance of an outside library being used
+    #Here I use a dictionary to mimic the structure of a dataframe
     df = {
         "X1": [],
         "X2": [],
         "Y": [],
     }
+
     X1_array = rng.integers(low=0, high=101, size=1000)
     X2_array = rng.integers(low=0, high=101, size=1000)
     Y_array = []
+
     for index, X1 in enumerate(X1_array):
         value = X1 + X2_array[index]
         Y_array.append(value)
     df["X1"] = X1_array
     df["X2"] = X2_array
     df["Y"] = Y_array
+
     return df
--- model.py
+++ model.py
@@ -2,11 +2,14 @@
 
 class Model:
     def __init__(self):
+        #Changing these nukes the model performance (if it can be fixed i probably will)
         self.W1 = 0.0
         self.W2 = 0.0
         self.bias = 0.0
-        self.learning_rate = 0.0001
 
+        self.learning_rate = 0.0001 #Ive come to know that if too high the model explodes
+
+    #Trains the model using the following pipeline: generates y_hat array -> calculate gradients -> adjust weights and bias -> repeat
     def train(self, epochs, X1_train, X2_train, Y_train):
         for x in range(epochs):
             self.predict(X1_train, X2_train)
@@ -19,14 +22,15 @@ class Model:
             self.W2 = self.W2 - self.learning_rate * gradient2
             self.bias = self.bias - self.learning_rate * gradient_bias
 
+    #This method generates an array with the model's results
     def predict(self, X1, X2):
         temp_y_hat = []
         for index, x in enumerate(X1):
-          #  y = self.W1 * X1 + self.W2 * X2[index] + self.bias
             y = self.W1 * x + self.W2 * X2[index] + self.bias
             temp_y_hat.append(y)
         self.y_hat = temp_y_hat
 
+    # Legacy from an old design mistake, keeping it to remember why i didnt use it
     #def mean_loss(self, Y_train):
     #    loss_function = 0
     #    for index, y_true in enumerate(Y_train):
@@ -61,10 +65,6 @@ class Model:
 
     def accuracy(self, X1_test, X2_test, Y_test):
 
-        print("W1:", self.W1)
-        print("W2:", self.W2)
-        print("bias:", self.bias)
-
         temp_y_hat = []
         count = 0
         for index, x in enumerate(X1_test):
@@ -73,7 +73,12 @@ class Model:
 
         for index, i in enumerate(temp_y_hat):
             z = Y_test[index]
-            if abs(i - z) < 0.01:
+            if abs(i - z) < 0.01: #Accuracy parameter as numbers will hardly be EXACTLY the same
                 count += 1
         accuracy = (count / len(X1_test)) * 100
         return accuracy
+
+    def stats(self): #You can use this to debug ig
+        print("W1: ", self.W1)
+        print("W2: ", self.W2)
+        print("bias: ", self.bias)
