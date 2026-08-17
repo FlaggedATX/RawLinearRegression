d89f613b03c4ee3d03159d9937df47cee7faff96
FlaggedATX <renatosegundo840@gmail.com>
2026-08-16 21:50:10 -0300

    chore: Explained dataset creation parameters

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from dataset import make_df
 from model import Model
 
 def main():
-    df_train = make_df()
+    # make_def() parameters: low->lowest number used, high->one above the highest number used, size->number of samples
+    df_train = make_df() 
     X1_train = df_train["X1"]
     X2_train = df_train["X2"]
     Y_train = df_train["Y"]
